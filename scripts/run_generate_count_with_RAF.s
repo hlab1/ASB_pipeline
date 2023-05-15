f63a44a9c21c6d9dcaf0c31d00276696d0dd8165
yz8868 <108205199+yz8868@users.noreply.github.com>
2023-05-15 18:03:48 -0400

    Update run_generate_count_with_RAF.s

--- scripts/run_generate_count_with_RAF.s
+++ scripts/run_generate_count_with_RAF.s
@@ -5,10 +5,6 @@
 #SBATCH --time=16:00:00
 #SBATCH --mem=128GB
 #SBATCH --job-name=count
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=yz8868@nyu.edu
-#SBATCH --output=slurm_count.out
-#SBATCH --error=slurm_count.err
 
 
 module load r/gcc/4.2.0
